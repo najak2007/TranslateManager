56113021424642d817db91251c78e1c792ea9997
Najak <najak2007@gmail.com>
2025-03-18 17:34:28 +0900

    초기화 문제점 수정

--- Sources/TranslateManager/TranslateManager.swift
+++ Sources/TranslateManager/TranslateManager.swift
@@ -8,14 +8,14 @@ import Translation
 class TranslateManager: NSObject {
     
     @MainActor @objc static let shared = TranslateManager()
-    @MainActor private static var viewModel = ViewModel()
+    @MainActor private var viewModel = ViewModel()
     
-
+    
     @objc static func preferences(langPacks: Array<String> = ["en"], completion: @escaping (String) -> Void) {      // Callback String - Error code
         
     }
     
-    @MainActor @objc static func getAvailableLangModels(completion: @escaping ([String]) -> Void) {
+    @MainActor @objc func getAvailableLangModels(completion: @escaping ([String]) -> Void) {
         var availableLanguages : Array<String> = []
         viewModel.availableLanguages.forEach { language in
             availableLanguages.append(language.localizedName())
